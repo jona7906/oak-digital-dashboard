38a69d6d44ec4def1fedf2f4dc16b3bc761ec073
lasse <lass948m@stud.kea.dk>
2022-05-12 14:55:25 +0200

    day end

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,57 +13,67 @@ function Dashboard(props) {
 // console.log(props.checksData.checks)
 {/* <PostResData server={check.serverName} timestamp={check.lasttesttime} restime={check.lastresponsetime}/>
  */}
-
+/* 
  const [dataRes, setRes] = useState([]);
  
 const uploadResData = () => props.checksData.checks.map((check)=>(
    PostResData(check.serverName, check.lasttesttime, check.lastresponsetime)
  ))
- useEffect(() => { uploadResData()}, []);
 
-/*  <PostResData data={props.checks} */
+ uploadResData()
+ useEffect(() => { setRes()}, []);
+ */
+ 
+ function getDate(serverdata) {
+  const findDate = serverdata.created;
+  const timestamp = new Date(findDate);  
+  let theDate = `${timestamp.getHours()}:${timestamp.getMinutes()} - ${timestamp.getDate()}/${timestamp.getMonth()+1}/${timestamp.getYear()}`;
+  return theDate;
+}
 
-/*  function statusIcon(){
- if(server.status === "up"){
-  console.log("IM UP");
- }
-} */
+const settings = {
+  sortBy: "response",
+  sortDir: "asc",
+};
+
+  function selectSort(event) {
+    // console.log(event)
+    // const sortBy = event.target.dataset.sort;
+    // const sortDir = event.target.dataset.sortDirection;
   
+    // //   Find old sortBy element, and remove .sortby
+    // const oldElement = document.querySelector(`[data-sort='${settings.sortBy}']`);
+    // oldElement.classList.remove("sortby");
+  
+    // //   Indicate active sort
+    // event.target.classList.add("sortby");
+  
+    // // Toggle the direction!
+    // if (sortDir === "asc") {
+    //   event.target.dataset.sortDirection = "desc";
+    // } else {
+    //   event.target.dataset.sortDirection = "asc";
+    // }
+    // console.log(`Sort by: ${sortBy} - ${sortDir}`);
+    // setSort(sortBy, sortDir);
+  }
 
- /*  const HtmlBody = () => {
-    
-    return(
-    props.checksData.checks.map((server)=>(
-      <div className="server">
-        <h1>{server.name}</h1>
-        <h2>host: {server.hostname}</h2>
-        <h2>status: {server.status}</h2>
-        <h2>type: {server.type}</h2>
-        <h2>res/time: {server.lastresponsetime}</h2>
-        <h2>resolution: {server.resolution}</h2>
-        
-      </div>
-      ))  ) ;
-   } */
+  // function setSort(sortBy, sortDir) {
+  //   settings.sortBy = sortBy;
+  //   settings.sortDir = sortDir;
+  // }
 
-   // for each loop? eventlistener? for hver server name hover funktion: fold ud så host bliver synligt og klikbart
-/* function handleClick() {
-  console
-} */
-
-   return (
-        
-    <div> 
-        {/* <h2>DASHBOARD</h2> */}
+    return (
+    <div className="dashboard-container"> 
         <div className="server-container">
         <div className="status-headlines">
-          <h2 className="status">STATUS</h2>
-          <h2 className="name">SITE NAME</h2>
-          <h2 className="host">HOST</h2>
-          <h2 className="type">TYPE</h2>
-          <h2 className="response">RESPONSE TIME</h2>
-          <h2 className="resolution">RESOLUTION</h2>
-          <h2 className="created">CREATED</h2>
+          <button onClick={selectSort(event)} data-action="sort" data-sort-direction="asc" data-sort="status">Status</button>
+          <button onClick={selectSort(event)} data-action="sort" data-sort-direction="asc" data-sort="site">Site</button>
+          <button onClick={selectSort(event)} data-action="sort" data-sort-direction="asc" data-sort="host">Host</button>
+          <button onClick={selectSort(event)} data-action="sort" data-sort-direction="asc" data-sort="type">Type</button>
+          <button onClick={selectSort(event)} data-action="sort" data-sort-direction="asc" data-sort="response">Response Time</button>
+          <button onClick={selectSort(event)} data-action="sort" data-sort-direction="asc" data-sort="resolution">Resolution</button>
+          <button onClick={selectSort(event)} data-action="sort" data-sort-direction="asc" data-sort="created">Created</button>
         </div>
         {props.checksData.checks.map((server)=>(
         <div className="server">
@@ -73,7 +83,7 @@ const uploadResData = () => props.checksData.checks.map((check)=>(
         <h2 className="server-type">{server.type}</h2>
         <h2 className="server-lastresponsetime">{server.lastresponsetime} ms</h2>
         <h2 className="server-resolution">{server.resolution}</h2>
-        <h2 className="server-resolution">{server.created}</h2>
+        <h2 className="server-resolution">{getDate(server)}</h2>
        </div>
        ))  } 
         </div>
